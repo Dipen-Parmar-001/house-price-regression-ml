e997ea98bc670a85d1a7ba2e2502925df2c21512
Dipen-Parmar-001 <pdipen000@gmail.com>
2026-03-02 09:02:44 +0530

    Include model performance metrics in training script
    Added model evaluation metrics: RMSE and R2 score.

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import joblib
+from sklearn.metrics import r2_score, mean_squared_error
+import numpy as np
+
 
 #Load Data From CSV-File
 
@@ -33,6 +36,17 @@ y = df['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
 
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+oob_score = model.oob_score_
+r2 = r2_score(y_test, y_pred)
+
+print(f"OOB R2: {oob_score}")
+print(f"RMSE: {rmse}")
+print(f"Test R2: {r2}")
 
 joblib.dump(model, "model.pkl")
 print("Training Completed. Model Saved.")
